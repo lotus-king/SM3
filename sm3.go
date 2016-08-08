8ef06e64e9c69606fd6eb0a282e1234dfea1cff9
Shi Taibin <hz_stb@163.com>
2016-08-08 16:37:43 +0800

    version 0.2

--- sm3.go
+++ sm3.go
@@ -1,21 +1,22 @@
 package sm3
 
 import (
-	// "bytes"
 	"encoding/binary"
 	"fmt"
-	// "hash"
+	"hash"
 )
 
 type SM3 struct {
-	digest   [8]uint32  // digest represents the partial evaluation of V
-	T        [64]uint32 // constant
-	message  []byte     // uint8  //
-	hashcode string     // hash result
+	digest      [8]uint32  // digest represents the partial evaluation of V
+	T           [64]uint32 // constant
+	unhandleMsg []byte     // uint8  //
+	// hashcode    string     // hash result
+	// offset      uint32     // offset of the last block
+	length uint64 // length of the message
 }
 
-func NewSM3(message []byte) *SM3 {
-	sm3 := &SM3{message: message, hashcode: ""}
+func NewSM3() hash.Hash {
+	sm3 := &SM3{length: 0}
 	sm3.digest[0] = 0x7380166f
 	sm3.digest[1] = 0x4914b2b9
 	sm3.digest[2] = 0x172442d7
@@ -32,61 +33,44 @@ func NewSM3(message []byte) *SM3 {
 	for i := 16; i < 64; i++ {
 		sm3.T[i] = 0x7a879d8a
 	}
-
-	// Padding
-	sm3.messagePad()
-
-	// reset hashcode
-	sm3.hashcode = ""
-
 	return sm3
 }
 
-func (sm3 *SM3) printMsg() {
-	for i := 0; i < len(sm3.message); i++ {
-		fmt.Printf("%02x", sm3.message[i])
-		if i%4 == 3 {
-			fmt.Printf(" ")
-		}
-		if i%(4*8) == 31 {
-			fmt.Println("")
-		}
-	}
-	fmt.Println("")
+// Reset clears the internal state by zeroing bytes in the state buffer.
+// This can be skipped for a newly-created hash state; the default zero-allocated state is correct.
+func (sm3 *SM3) Reset() {
+	// Reset digest
+	sm3.digest[0] = 0x7380166f
+	sm3.digest[1] = 0x4914b2b9
+	sm3.digest[2] = 0x172442d7
+	sm3.digest[3] = 0xda8a0600
+	sm3.digest[4] = 0xa96f30bc
+	sm3.digest[5] = 0x163138aa
+	sm3.digest[6] = 0xe38dee4d
+	sm3.digest[7] = 0xb0fb0e4e
+
+	// Reset numberic states
+	sm3.length = 0
+	// sm3.offset = 0
+
+	// Reset
+	// sm3.hashcode = ""
 }
 
-func (sm3 *SM3) messagePad() {
-	fmt.Println("messagePad------->")
-	fmt.Println("Before padding:")
-	sm3.printMsg()
+// BlockSize, required by the hash.Hash interface.
+// BlockSize returns the hash's underlying block size.
+// The Write method must be able to accept any amount
+// of data, but it may operate more efficiently if all writes
+// are a multiple of the block size.
+func (sm3 *SM3) BlockSize() int {
+	// Here return the number of byte
+	return 64
+}
 
-	msgLen := uint64(len(sm3.message) * 8)
-
-	// append '1'
-	sm3.message = append(sm3.message, 0x80)
-
-	// append until the resulting message length (in bits) is congruent to 448 (mod 512)
-	blockSize := 64
-	for len(sm3.message)%blockSize != 56 {
-		sm3.message = append(sm3.message, 0x00)
-	}
-
-	// append message length
-	sm3.message = append(sm3.message, uint8(msgLen>>56&0xff))
-	sm3.message = append(sm3.message, uint8(msgLen>>48&0xff))
-	sm3.message = append(sm3.message, uint8(msgLen>>40&0xff))
-	sm3.message = append(sm3.message, uint8(msgLen>>32&0xff))
-	sm3.message = append(sm3.message, uint8(msgLen>>24&0xff))
-	sm3.message = append(sm3.message, uint8(msgLen>>16&0xff))
-	sm3.message = append(sm3.message, uint8(msgLen>>8&0xff))
-	sm3.message = append(sm3.message, uint8(msgLen>>0&0xff))
-
-	if len(sm3.message)%64 != 0 {
-		fmt.Println("------messagePad: Error, length =", len(sm3.message))
-	}
-
-	fmt.Println("After padding:")
-	sm3.printMsg()
+// Size, required by the hash.Hash interface.
+// Size returns the number of bytes Sum will return.
+func (sm3 *SM3) Size() int {
+	return 32
 }
 
 func (sm3 *SM3) ff0(x, y, z uint32) uint32 {
@@ -161,16 +145,16 @@ func (sm3 *SM3) cf(W [68]uint32, W1 [64]uint32) {
 		F = E
 		E = sm3.p0(TT2)
 
-		// debug
-		fmt.Printf("%02d: ", i)
-		fmt.Printf("%08x ", A)
-		fmt.Printf("%08x ", B)
-		fmt.Printf("%08x ", C)
-		fmt.Printf("%08x ", D)
-		fmt.Printf("%08x ", E)
-		fmt.Printf("%08x ", F)
-		fmt.Printf("%08x ", G)
-		fmt.Printf("%08x\n", H)
+		// // debug
+		// fmt.Printf("%02d: ", i)
+		// fmt.Printf("%08x ", A)
+		// fmt.Printf("%08x ", B)
+		// fmt.Printf("%08x ", C)
+		// fmt.Printf("%08x ", D)
+		// fmt.Printf("%08x ", E)
+		// fmt.Printf("%08x ", F)
+		// fmt.Printf("%08x ", G)
+		// fmt.Printf("%08x\n", H)
 	}
 
 	for i := 16; i < 64; i++ {
@@ -188,15 +172,15 @@ func (sm3 *SM3) cf(W [68]uint32, W1 [64]uint32) {
 		E = sm3.p0(TT2)
 
 		// debug
-		fmt.Printf("%02d: ", i)
-		fmt.Printf("%08x ", A)
-		fmt.Printf("%08x ", B)
-		fmt.Printf("%08x ", C)
-		fmt.Printf("%08x ", D)
-		fmt.Printf("%08x ", E)
-		fmt.Printf("%08x ", F)
-		fmt.Printf("%08x ", G)
-		fmt.Printf("%08x\n", H)
+		// fmt.Printf("%02d: ", i)
+		// fmt.Printf("%08x ", A)
+		// fmt.Printf("%08x ", B)
+		// fmt.Printf("%08x ", C)
+		// fmt.Printf("%08x ", D)
+		// fmt.Printf("%08x ", E)
+		// fmt.Printf("%08x ", F)
+		// fmt.Printf("%08x ", G)
+		// fmt.Printf("%08x\n", H)
 	}
 
 	sm3.digest[0] ^= A
@@ -209,47 +193,154 @@ func (sm3 *SM3) cf(W [68]uint32, W1 [64]uint32) {
 	sm3.digest[7] ^= H
 }
 
-func (sm3 *SM3) Hash() string {
-	blockSize := 64
-	nblocks := len(sm3.message) / blockSize
-
+// update, iterative compress, update digests
+func (sm3 *SM3) update(msg []byte, nblocks int) {
 	for i := 0; i < nblocks; i++ {
-		startPos := i * blockSize
-		W, W1 := sm3.messageExtend(sm3.message[startPos : startPos+blockSize])
+		startPos := i * sm3.BlockSize()
+		W, W1 := sm3.messageExtend(msg[startPos : startPos+sm3.BlockSize()])
 
 		// debug
-		printUint32Slice(W[:])
-		printUint32Slice(W1[:])
+		// printUint32Slice(W[:])
+		// printUint32Slice(W1[:])
 
 		sm3.cf(W, W1)
 	}
-
-	// Transform sm3.digest[] to hashcode string
-	sm3.constructHashCode()
-
-	sm3.printValues()
-
-	return sm3.hashcode
 }
 
-func (sm3 *SM3) printValues() {
-	for i := 0; i < 8; i++ {
-		fmt.Printf("%x ", sm3.digest[i])
+// Write, required by the hash.Hash interface.
+// Write (via the embedded io.Writer interface) adds more data to the running hash.
+// It never returns an error.
+func (sm3 *SM3) Write(p []byte) (int, error) {
+	// fmt.Println("Write---------------->")
+	toWrite := len(p)
+	sm3.length += uint64(len(p) * 8)
+	// fmt.Println("new len:", sm3.length)
+
+	msg := append(sm3.unhandleMsg, p...)
+	nblocks := len(msg) / sm3.BlockSize()
+	sm3.update(msg, nblocks)
+
+	// Update unhandleMsg
+	sm3.unhandleMsg = msg[nblocks*sm3.BlockSize():]
+
+	return toWrite, nil
+}
+
+func (sm3 *SM3) pad() []byte {
+	fmt.Println("pad------->")
+	fmt.Println("message length:", sm3.length, "bits")
+
+	// Debug
+	// fmt.Println("Before padding:")
+	// sm3.printMsg()
+
+	// Make a copy not using unhandleMsg
+	msg := sm3.unhandleMsg
+
+	// Append '1'
+	msg = append(msg, 0x80)
+
+	// Append until the resulting message length (in bits) is congruent to 448 (mod 512)
+	blockSize := 64
+	for len(msg)%blockSize != 56 {
+		msg = append(msg, 0x00)
 	}
-	fmt.Printf("\n")
-	// fmt.Println(sm3.hashcode)
-}
 
-func (sm3 *SM3) constructHashCode() {
-	fmt.Println("constructHashCode------------------------->")
-	for i := 0; i < 8; i++ {
-		sm3.hashcode += fmt.Sprintf("%x", sm3.digest[i])
+	// append message length
+	msg = append(msg, uint8(sm3.length>>56&0xff))
+	msg = append(msg, uint8(sm3.length>>48&0xff))
+	msg = append(msg, uint8(sm3.length>>40&0xff))
+	msg = append(msg, uint8(sm3.length>>32&0xff))
+	msg = append(msg, uint8(sm3.length>>24&0xff))
+	msg = append(msg, uint8(sm3.length>>16&0xff))
+	msg = append(msg, uint8(sm3.length>>8&0xff))
+	msg = append(msg, uint8(sm3.length>>0&0xff))
+
+	if len(msg)%64 != 0 {
+		fmt.Println("------pad: Error, msgLen =", len(msg))
 	}
-	fmt.Println(sm3.hashcode)
+
+	// fmt.Println("After padding:")
+	// print msg
+	return msg
 }
 
-func (sm3 *SM3) GetHashCode() string {
-	return sm3.hashcode
+// Sum, required by the hash.Hash interface.
+// Sum appends the current hash to b and returns the resulting slice.
+// It does not change the underlying hash state.
+func (sm3 *SM3) Sum(in []byte) []byte {
+	msg := sm3.pad()
+
+	// Finialize
+	sm3.update(msg, len(msg)/sm3.BlockSize())
+
+	// save hash to in
+	needed := sm3.Size()
+	if cap(in)-len(in) < needed {
+		fmt.Println("---------------------Should not happen here.")
+		newIn := make([]byte, len(in), len(in)+needed)
+		copy(newIn, in)
+		in = newIn
+	}
+	out := in[len(in) : len(in)+needed]
+
+	for i := 0; i < 8; i++ {
+		binary.BigEndian.PutUint32(out[i*4:], sm3.digest[i])
+	}
+
+	// debug
+	// sm3.printValues()
+
+	return out
+}
+
+// func (sm3 *SM3) Hash() string {
+// 	blockSize := 64
+// 	nblocks := len(sm3.message) / blockSize
+
+// 	for i := 0; i < nblocks; i++ {
+// 		startPos := i * blockSize
+// 		W, W1 := sm3.messageExtend(sm3.message[startPos : startPos+blockSize])
+
+// 		// debug
+// 		printUint32Slice(W[:])
+// 		printUint32Slice(W1[:])
+
+// 		sm3.cf(W, W1)
+// 	}
+
+// 	// Transform sm3.digest[] to hashcode string
+// 	sm3.constructHashCode()
+
+// 	sm3.printValues()
+
+// 	return sm3.hashcode
+// }
+
+// func (sm3 *SM3) constructHashCode() {
+// 	fmt.Println("constructHashCode------------------------->")
+// 	for i := 0; i < 8; i++ {
+// 		sm3.hashcode += fmt.Sprintf("%x", sm3.digest[i])
+// 	}
+// 	fmt.Println(sm3.hashcode)
+// }
+
+// func (sm3 *SM3) GetHashCode() string {
+// 	return sm3.hashcode
+// }
+
+//------------------ ALL debug functions
+func (sm3 *SM3) printMsg() {
+	for i := 0; i < len(sm3.unhandleMsg); i++ {
+		fmt.Printf("%02x", sm3.unhandleMsg[i])
+		if i%4 == 3 {
+			fmt.Printf(" ")
+		}
+		if i%(4*8) == 31 {
+			fmt.Println("")
+		}
+	}
+	fmt.Println("")
 }
 
 func printUint32Slice(list []uint32) {
@@ -260,4 +351,12 @@ func printUint32Slice(list []uint32) {
 		}
 	}
 	fmt.Println("")
+}
+
+func (sm3 *SM3) printValues() {
+	for i := 0; i < 8; i++ {
+		fmt.Printf("%x ", sm3.digest[i])
+	}
+	fmt.Printf("\n")
+	// fmt.Println(sm3.hashcode)
 }
--- sm3_test.go
+++ sm3_test.go
@@ -1,6 +1,9 @@
 package sm3
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestSM3_1(t *testing.T) {
 	msg := "abc"
@@ -8,40 +11,55 @@ func TestSM3_1(t *testing.T) {
 	trueVal := "66c7f0f462eeedd9d1f2d46bdc10e4e24167c4875cf2f7a2297da02b8f4ba8e0"
 
 	copy(buffer[:], msg)
-	sm3 := NewSM3(buffer[:])
-	calcVal := sm3.Hash()
+	hw := NewSM3()
+	hw.Write(buffer[:])
+
+	uhash := make([]uint8, 32)
+	hw.Sum(uhash[:0])
+	calcVal := Byte2String(uhash)
+
 	if calcVal != trueVal {
-		t.Errorf("Expected: %v,\nbut : %v\n", trueVal, calcVal)
+		t.Errorf("expected: %x,\nbut got: %v\n", trueVal, calcVal)
 	}
 }
 
 func TestSM3_2(t *testing.T) {
-	str := "abcd"
-	msg := ""
-	for i := 0; i < 16; i++ {
-		msg += str
-	}
-
+	msg := "abcd"
+	var buffer [4]byte
 	trueVal := "debe9ff92275b8a138604889c18e5a4d6fdb70e5387e5765293dcba39c0c5732"
 
-	var buffer [64]byte
 	copy(buffer[:], msg)
-	sm3 := NewSM3(buffer[:])
-	calcVal := sm3.Hash()
+	hw := NewSM3()
+	for i := 0; i < 16; i++ {
+		hw.Write(buffer[:])
+	}
+
+	uhash := make([]uint8, 32)
+	hw.Sum(uhash[:0])
+	calcVal := Byte2String(uhash)
+
 	if calcVal != trueVal {
-		t.Errorf("Expected: %v,\nbut : %v\n", trueVal, calcVal)
+		t.Errorf("expected: %x,\nbut got: %v\n", trueVal, calcVal)
 	}
 }
 
-// func TestSM3(t *testing.T) {
-// 	str := "abcd"
-// 	msg := ""
-// 	for i := 0; i < 16; i++ {
-// 		msg += str
-// 	}
+func Byte2String(b []byte) string {
+	ret := ""
+	for i := 0; i < len(b); i++ {
+		ret += fmt.Sprintf("%02x", b[i])
+	}
+	return ret
+}
 
-// 	var buffer [64]byte
-// 	copy(buffer[:], msg)
-// 	sm3 := NewSM3(buffer[:])
-// 	sm3.Hash()
-// }
+func printByteSlice(list []byte) {
+	for i := 0; i < len(list); i++ {
+		fmt.Printf("%02x", list[i])
+		if i%4 == 3 {
+			fmt.Printf(" ")
+		}
+		if i%(4*8) == 31 {
+			fmt.Println("")
+		}
+	}
+	fmt.Println("")
+}
